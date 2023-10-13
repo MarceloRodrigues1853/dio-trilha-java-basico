bb9874c5b78671b07087ebf97538c48002118402
MarceloRodrigues1853 <marcelo180886@gmail.com>
2023-10-13 16:36:23 -0300

    atualização

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,29 +1,18 @@
 public class Operadores {
 
     public static void main(String[] args) {
+        // ComparacaoAvancada.java
+        int numero1 = 1;
+        int numero2 = 1;
 
-        // qual o resultado das expressoes abaixo?
-        String concatenacao = "?";
+        if (numero1 == 2 & numero2++ == 2)
+            System.out.println("As 2 condições são verdadeiras");
 
-        concatenacao = 1 + 1 + 1 + "1";
-        
-        System.out.println(concatenacao);
+        System.out.println("O numero 1 agora é " + numero1);
+        System.out.println("O numero 2 agora é " + numero2);
 
-        concatenacao = 1 + "1" + 1 + 1;
+        // Vamos mudar a linha 5 do código acima para: if(numero1== 2 && numero2 ++ == 2
+        // )
 
-        System.out.println(concatenacao);
-
-        concatenacao = 1 + "1" + 1 + "1";
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + 1 + 1 + 1;
-
-        System.out.println(concatenacao);
-
-        concatenacao = "1" + (1 + 1 + 1);
-
-        System.out.println(concatenacao);
-        
     }
 }

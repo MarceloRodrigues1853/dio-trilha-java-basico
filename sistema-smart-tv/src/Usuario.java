858bb19175de0a6c61ff1732ca2d101f344d59c1
MarceloRodrigues1853 <marcelo180886@gmail.com>
2023-10-14 15:45:52 -0300

    atualização

--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -3,8 +3,25 @@ public class Usuario {
         
         SmartTv smartTv = new SmartTv();
 
-        System.out.println("TV ligagada ? " + smartTv.ligada);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume Atual :" + smartTv.volume);
+
         System.out.println("Canal Atual :" + smartTv.canal);
-        System.out.println("Volume Atual :" + smartTv.ligada);
+        smartTv.mudarCanal(13);
+
+        System.out.println("Canal Atual :" + smartTv.canal);
+
+        System.out.println("TV Ligada ? " + smartTv.ligada);
+        System.out.println("Canal Atual :" + smartTv.canal);
+        System.out.println("Volume Atual :" + smartTv.volume);
+
+        smartTv.ligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
+
+        smartTv.desligar ();
+        System.out.println("Novo Status -> TV ligada ? " + smartTv.ligada);
     }
 }
